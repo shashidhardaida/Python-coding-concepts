59d1e22f0b2cdb896889c458c5a2354fe18b5690
shashi1995 <39859055+shashi1995@users.noreply.github.com>
2019-12-06 14:00:23 +0100

    Create ConditionalStatements.py

---
+++ ConditionalStatements.py
@@ -0,0 +1,12 @@
+                                                            # --: The if statement :-- # 
+
+if 2 > 1:
+    print("This statement is true")  # Here the indentation of print statement is important. Because in Python space is very important
+# In the above statement if there is no atleast one space for print statement inside if loop it will thrwo Indentation error.
+# The recommended number of spaces to indent a block of code is four.
+var1 = 2
+var2 = 3
+if var1 > var2:
+    print("This statement is also true")
+else:
+    print("This is not true")
